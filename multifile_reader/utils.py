47c7831a20e02ed69648c915a93a2162b7303e5b
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2020-03-26 17:27:44 +0100

    special import for py27 of urlparse

--- multifile_reader/utils.py
+++ multifile_reader/utils.py
@@ -1,5 +1,8 @@
 """Various utils used for MultiFileReader."""
-from urllib.parse import urlparse
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 import requests
 
